4e0f0a25c7eeb59d3e6ff11e7cacc598d29b80dd
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-04-22 10:40:49 -0300

    Implementa Request

--- src/Core/Request.php
+++ src/Core/Request.php
@@ -1,5 +1,34 @@
 <?php 
-    // namespace 
+    namespace PAW\Core;
+
+    class Request {
+        private static $instance;
+
+        public static function getInstance()
+        {
+            if (!isset(self::$instance)) {
+                self::$instance = new self();
+            }
+    
+            return self::$instance;
+        }
+        
+        public function path () {
+            return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        }
+        public function httpMethod () {
+            return parse_url($_SERVER['REQUEST_METHOD'], PHP_URL_PATH);
+        }
+
+        public function route() {
+            return [$this -> path(), $this -> httpMethod()];
+        }
+        
+        public function getKey(string $key) {
+            return $_POST[$key] ?? $_GET[$key] ?? null;
+
+        }
+    }
 
 
 ?>
