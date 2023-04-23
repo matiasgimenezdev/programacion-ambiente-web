ddf8548b03f466ff80d18ef03c06ee2e44cd0612
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-04-23 12:28:34 -0300

    Refactoriza el Router

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -2,15 +2,21 @@
     namespace PAW\Core;
     use PAW\Core\Exceptions\RouteNotFoundException;
 
-    class Router{
+    class Router {
         private static $instance;
 
-        public $routes = [
-            "GET" => [
-                "/" => ["PageController", "home"]
-            ],
+        private $routes = [
+            "GET" => [],
             "POST" => []
         ];
+
+        private $notFound = "/not-found";
+        private $internalError = "/internal-error";
+
+        private function __construct () {
+            $this -> get($this -> notFound, "ErrorController","notFound");
+            $this -> get($this -> internalError, "ErrorController","internalError");
+        }
 
         public static function getInstance()
         {
@@ -22,25 +28,40 @@
         }
 
         public function direct($request) {
-            list($path, $httpMethod) = $request -> route();
+            try{
+                list($path, $httpMethod) = $request -> route();
+                list($controller, $method) = $this -> getController($path, $httpMethod);
+            } catch(RouteNotFoundException $exception){
+                list($controller, $method) = $this -> getController($this -> notFound, "GET");
+            } catch(Exception $exception) {
+                list($controller, $method) = $this -> getController($this -> internalError, "GET");
+            } finally {
+                $this -> invokeController($controller, $method);
+            }
+
+        }
+
+        private function getController($path, $httpMethod) {
             if (!$this -> exists($path, $httpMethod)) {
                 throw new RouteNotFoundException("There is no route for that path");
             }
-
             list($controller, $method) = $this -> routes[$httpMethod][$path];
+            return [$controller, $method];
+        }
 
+        private function invokeController($controller, $method){
             $controllerName = "PAW\\App\\Controllers\\{$controller}";
             $controllerInstance = new $controllerName;
             $controllerInstance -> $method();
-
         }
 
+
         public function post($path, $controller, $method) {
-            // loadRoute($path, $controller, $method, "POST");
+            $this -> loadRoute($path, $controller, $method, "POST");
         }
 
         public function get($path, $controller, $method) {
-            // loadRoute($path, $controller, $method, "GET");
+            $this -> loadRoute($path, $controller, $method, "GET");
         }
 
         private function exists($path, $httpMethod) {
@@ -48,7 +69,7 @@
         }
 
         private function loadRoute($path, $controller, $method, $httpMethod){
-            // $this -> routes[$httpMethod][$path] = [$controller, $method];
+            $this -> routes[$httpMethod][$path] = [$controller, $method];
         }
 
     }
