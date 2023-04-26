c1e841c7c3664dc9943a6e5dc447802ffed52169
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-04-26 10:33:56 -0300

    Agrega enlace a la noticia

--- src/App/Views/Fragments/noticias-list.view.php
+++ src/App/Views/Fragments/noticias-list.view.php
@@ -1,12 +1,16 @@
 <?php if($title === "Noticias"): ?>
     <?php foreach($noticias as $index => $noticia): ?>
         <article class="<?php echo ($index == 0 ? "new head" : "new")?>"> 
-            <img src="<?= $noticia -> getImg() ?>" alt="<?= $noticia -> getImgAlt() ?>" width="300" height="200" />
-            <h3><?= $noticia -> getTitle() ?></h3>
+            <a href="<?= "/noticia?id=". $noticia -> getId() ?>">
+                <img src="<?= $noticia -> getImg() ?>" alt="<?= $noticia -> getImgAlt() ?>" width="300" height="200" />
+            </a>
+            <a href="<?= "/noticia?id=" . $noticia -> getId() ?>"> 
+                <h3><?= $noticia -> getTitle() ?></h3>
+            </a>
             <p>
                 <?= $noticia -> getDescription() ?>
             </p>
-        </article>       
+        </article>
     <?php endforeach;?>
 <?php endif;?>
 
