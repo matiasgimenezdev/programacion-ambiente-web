f5456e12c8904ab753aa1294fae7d4da15995a8b
SimonDiLeoGIT <saimondiley@gmail.com>
2023-05-12 18:32:04 -0300

    Agrega tabla widget

--- public/scripts/components/turnos-widget/TurnosWidget.js
+++ public/scripts/components/turnos-widget/TurnosWidget.js
@@ -4,6 +4,7 @@ export class TurnosWidget {
 
     const url = 'scripts/components/turnos-widget/turnos.json';
 
+
     this.obtenerProfesionales(url);
   }
 
@@ -16,28 +17,91 @@ export class TurnosWidget {
   }
   
   datosProfesional(profesionales) {
-    const medicoSeleccionado = document.querySelector('.medico');
-    medicoSeleccionado.addEventListener("change", function() {
+    const medicoSeleccionado = document.querySelector(".medico");
+    medicoSeleccionado.addEventListener("change", () => {
       const medico = profesionales.find(
         (profesional => 
           profesional.matricula == medicoSeleccionado.value)
       );
+      let tabla = document.createElement('table');
+      //this.setDias(tabla);
+      //this.setHoras(tabla, medico);
+      const container = document.querySelector(".medicos");
+      container.appendChild(tabla);
+
+      let fecha = new Date();
+    
+      let encabezado = tabla.createTHead();
+      let fila = encabezado.insertRow();
+      let opciones = { weekday: 'long', day: 'numeric' };
+      let i = 0;
+      while (i < 7) {
+        if (fecha.getDay() !== 6 && fecha.getDay() !== 0) {
+          let celda = fila.insertCell();
+          celda.innerText = fecha.toLocaleDateString('es-ES', opciones);
+          i++;
+        }
+        fecha.setDate(fecha.getDate() + 1);
+      }
+
+      let horaInicio = medico.horarioInicio.horas;
+      let horaFin = medico.horarioFinalizacion.horas;
+      let hora = horaInicio;
+      while (hora <= horaFin) 
+      {
+        let fila = tabla.insertRow();
+        let i = 0;
+        while (i < 7) {
+            let celda = fila.insertCell();
+            celda.innerText = hora;
+            i++;
+        }
+        hora++;
+      }
     });
-    this.setDias();
   }
 
-  setDias() {
-    
-    const date = document.querySelector("#fecha-turno");
-    
-    const fechaActual = new Date();
-    const fechaLimite = new Date();
-    fechaLimite.setDate(fechaActual.getDate() + 7); // Límite de 7 días desde hoy
+  armarTabla(medico) {
 
-    date.min = fechaActual.toISOString().slice(0, 10);
-    date.max = fechaLimite.toISOString().slice(0, 10);
-
+    let tabla = document.createElement('table');
+    this.setDias(tabla);
+    this.setHoras(tabla, medico);
+    const container = document.querySelector(".medicos");
+    container.appendChild(tabla);
   }
 
+  setDias(tabla) {
+    let fecha = new Date();
+    
+    let encabezado = tabla.createTHead();
+    let fila = encabezado.insertRow();
+    let opciones = { weekday: 'long', day: 'numeric' };
+    let i = 0;
+    while (i < 7) {
+      if (fecha.getDay() !== 6 && fecha.getDay() !== 0) {
+        let celda = fila.insertCell();
+        celda.innerText = fecha.toLocaleDateString('es-ES', opciones);
+        i++;
+      }
+      fecha.setDate(fecha.getDate() + 1);
+    }
+  }
+
+  setHoras(tabla, medico) {
+    let horaInicio = medico.horarioInicio.horas;
+    let horaFin = medico.horarioFinalizacion.horas;
+    let hora = horaInicio;
+    while (hora <= horaFin) 
+    {
+      let fila = tabla.insertRow();
+      let i = 0;
+      while (i < 7) {
+          let celda = fila.insertCell();
+          celda.innerText = hora;
+          i++;
+      }
+      hora++;
+    }
+  }
 
 }
