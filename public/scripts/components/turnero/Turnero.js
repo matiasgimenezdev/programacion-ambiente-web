1680fbe88821b24ca3489b2bc7246333571c7e60
SimonDiLeoGIT <saimondiley@gmail.com>
2023-05-18 19:42:42 -0300

    Calculo de tiempo de espera en paciente view

--- public/scripts/components/turnero/Turnero.js
+++ public/scripts/components/turnero/Turnero.js
@@ -61,6 +61,9 @@ export class Turnero {
         }
       );
 
+      this.$paciente = "Hernán";
+      this.$idTurno = "D14";
+
       setInterval(() => {
         const $url = 'scripts/components/turnero/turnero.json';
         fetch($url)
@@ -68,7 +71,16 @@ export class Turnero {
           .then((turnos) => {
             this.$turnosRequest = turnos.turnos;
             this.$turnos = this.$turnosRequest;
-            this.setTurnos(this.$turnos);
+            this.setTurnos(this.$turnos, this.$paciente);
+            
+            fetch('scripts/components/turnero/turnos.json')
+              .then((response) => response.json())
+              .then((data) => {
+                this.$profesionales = data.especialistas;
+                this.calcularTiempoDeEspera(this.$profesionales, this.$turnos, this.$idTurno);
+              }
+            );
+
           }
         );
       }, 10000);
@@ -191,7 +203,7 @@ export class Turnero {
   }
 
 
-  setTurnos($turnos){
+  setTurnos($turnos, $paciente){
 
     const turnoActual = document.querySelector(".actual");
 
@@ -208,9 +220,42 @@ export class Turnero {
     for(let i = 1; i <= limit; i++){
       const turno = document.createElement("p");
       turno.textContent = $turnos[i].id;
+      if($turnos[i].nombre == $paciente){
+        turno.classList.add("su-turno");
+        const turnoPaciente = document.querySelector(".turno-user");
+        turnoPaciente.textContent = "Su turno: " + $turnos[i].id;
+      }
       colaContainer.appendChild(turno);
     }
 
+  }
+
+
+  calcularTiempoDeEspera($profesionales, $turnos, $idTurno){
+
+    let tiempoEstimado = 0;
+
+    for(let i = 0; i <= $turnos.length; i++){
+      console.log($turnos[i].id);
+      if($turnos[i].id == $idTurno){
+        const profesional = $turnos[i].profesional;
+        let partes = profesional.split(" ");
+        let nombre = partes[0];
+        let apellido = partes[1];
+
+        for(let j = 0; j <= $profesionales.length; j++){
+          if($profesionales[j].nombre == nombre && $profesionales[j].apellido == apellido){
+            let tiempoPorTurno = $profesionales[j].duracionTurno;
+            tiempoEstimado = tiempoPorTurno * i;
+            break;
+          }
+        }
+        break;
+      }
+    }
+
+    const tiempoDeEsperaEstimado = document.querySelector(".tiempo-espera");
+    tiempoDeEsperaEstimado.textContent = "Tiempo de espera estimado: " + tiempoEstimado + " minutos";
   }
 
 
