0ca93eb5e451811543abce1976a21f414fdb638b
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-05-19 08:51:37 -0300

    Fix bug en app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,73 +8,90 @@ import { ProfesionalesFilter } from './components/profesionales-filter/profesion
 import { TurnosFilter } from './components/turnos-filter/turnos-filter.js';
 
 const routes = {
-	'/solicitar-turno': {
-		scriptName: 'drag-drop',
-		scriptPath: 'scripts/components/drag-drop/drag-drop.js',
-		initFunction: () => new DragAndDrop(),
-	},
-	'/solicitar-turno': {
-		scriptName: 'TurnosWidget',
-		scriptPath: 'scripts/components/turnos-widget/TurnosWidget.js',
-		initFunction: () => new TurnosWidget(),
-	},
-	'/': {
-		scriptName: 'carousel',
-		scriptPath: 'scripts/components/carousel/carousel.js',
-		initFunction: () => {
-			const $container = document.querySelector('.carousel-list');
-			new Carousel($container);
+	'/solicitar-turno': [
+		{
+			scriptName: 'drag-drop',
+			scriptPath: 'scripts/components/drag-drop/drag-drop.js',
+			initFunction: () => new DragAndDrop(),
 		},
-	},
-	'/especialidades': {
-		scriptName: 'especialidades-filter',
-		scriptPath:
-			'scripts/components/especialidades-filter/especialidades-filter.js',
-		initFunction: () => new EspecialidadesFilter(),
-	},
-	'/especialidad-search': {
-		scriptName: 'especialidades-filter',
-		scriptPath:
-			'scripts/components/especialidades-filter/especialidades-filter.js',
-		initFunction: () => new EspecialidadesFilter(),
-	},
-	'/turnero': {
-		scriptName: 'Turnero',
-		scriptPath: 'scripts/components/turnero/Turnero.js',
-		initFunction: () => new Turnero('paciente'),
-	},
+		{
+			scriptName: 'TurnosWidget',
+			scriptPath: 'scripts/components/turnos-widget/TurnosWidget.js',
+			initFunction: () => new TurnosWidget(),
+		},
+	],
+	'/': [
+		{
+			scriptName: 'carousel',
+			scriptPath: 'scripts/components/carousel/carousel.js',
+			initFunction: () => {
+				const $container = document.querySelector('.carousel-list');
+				new Carousel($container);
+			},
+		},
+	],
+	'/especialidades': [
+		{
+			scriptName: 'especialidades-filter',
+			scriptPath:
+				'scripts/components/especialidades-filter/especialidades-filter.js',
+			initFunction: () => new EspecialidadesFilter(),
+		},
+	],
+	'/especialidad-search': [
+		{
+			scriptName: 'especialidades-filter',
+			scriptPath:
+				'scripts/components/especialidades-filter/especialidades-filter.js',
+			initFunction: () => new EspecialidadesFilter(),
+		},
+	],
+	'/turnero': [
+		{
+			scriptName: 'Turnero',
+			scriptPath: 'scripts/components/turnero/Turnero.js',
+			initFunction: () => new Turnero('paciente'),
+		},
+	],
 
-	'/profesionales': {
-		scriptName: 'profesionales-filter',
-		scriptPath:
-			'scripts/components/profesionales-filter/profesionales-filter.js',
-		initFunction: () => new ProfesionalesFilter(),
-	},
-	'/profesionales-search': {
-		scriptName: 'profesionales-filter',
-		scriptPath:
-			'scripts/components/profesionales-filter/profesionales-filter.js',
-		initFunction: () => new ProfesionalesFilter(),
-	},
-	'/turnos': {
-		scriptName: 'turnos-filter',
-		scriptPath: 'scripts/components/turnos-filter/turnos-filter.js',
-		initFunction: () => new TurnosFilter(),
-	},
+	'/profesionales': [
+		{
+			scriptName: 'profesionales-filter',
+			scriptPath:
+				'scripts/components/profesionales-filter/profesionales-filter.js',
+			initFunction: () => new ProfesionalesFilter(),
+		},
+	],
+	'/profesionales-search': [
+		{
+			scriptName: 'profesionales-filter',
+			scriptPath:
+				'scripts/components/profesionales-filter/profesionales-filter.js',
+			initFunction: () => new ProfesionalesFilter(),
+		},
+	],
+	'/turnos': [
+		{
+			scriptName: 'turnos-filter',
+			scriptPath: 'scripts/components/turnos-filter/turnos-filter.js',
+			initFunction: () => new TurnosFilter(),
+		},
+	],
 };
 
 document.addEventListener('DOMContentLoaded', () => {
 	const currentPath = window.location.pathname;
+	for (const route in routes) {
+		const scripts = routes[route];
+		for (const script of scripts) {
+			if (
+				route === currentPath ||
+				(route !== '/' && currentPath.includes(route))
+			) {
+				const { scriptName, scriptPath, initFunction } = script;
 
-	for (const [
-		route,
-		{ scriptName, scriptPath, initFunction },
-	] of Object.entries(routes)) {
-		if (
-			currentPath === route ||
-			(route !== '/' && currentPath.includes(route))
-		) {
-			ScriptLoader.loadScript(scriptName, scriptPath, initFunction);
+				ScriptLoader.loadScript(scriptName, scriptPath, initFunction);
+			}
 		}
 	}
 });
