0571582d2e7cd005726f37ee22efdef390205f49
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-06-03 12:06:20 -0300

    Agrega carga de obras sociales en solicitar turno

--- src/App/Models/ObraSocial/ObrasSocialesCollection.php
+++ src/App/Models/ObraSocial/ObrasSocialesCollection.php
@@ -6,41 +6,43 @@ use PAW\Core\Model;
 
 class ObrasSocialesCollection extends Model
 {
+  public $table = 'obra_social';
 
   public function getAll()
   {
-    $obrasSociales = [
-      [
-        "id" => 1,
-        "name" => "Swiss Medical",
-        "img" => "assets/images/obras-sociales/SwissMedical.svg",
-      ],
-      [
-        "id" => 2,
-        "name" => "Jerarquicos Salud",
-        "img" => "assets/images/obras-sociales/JerarquicosSalud.png",
-      ],
-      [
-        "id" => 3,
-        "name" => "IOMA - Instituto Obra Médico Asistencial",
-        "img" => "assets/images/obras-sociales/IOMA.png",
-      ],
-      [
-        "id" => 4,
-        "name" => "OSDE - Organización de Servicios Directos Empresarios",
-        "img" => "assets/images/obras-sociales/OSDE.png",
-      ],
-      [
-        "id" => 5,
-        "name" => "PAMI - Programa de Atención Médica Integral",
-        "img" => "assets/images/obras-sociales/PAMI.svg",
-      ],
-      [
-        "id" => 6,
-        "name" => "Medife",
-        "img" => "assets/images/obras-sociales/Medife.jpg",
-      ]
-    ];
+    // $obrasSociales = [
+    //   [
+    //     "id" => 1,
+    //     "name" => "Swiss Medical",
+    //     "img" => "assets/images/obras-sociales/SwissMedical.svg",
+    //   ],
+    //   [
+    //     "id" => 2,
+    //     "name" => "Jerarquicos Salud",
+    //     "img" => "assets/images/obras-sociales/JerarquicosSalud.png",
+    //   ],
+    //   [
+    //     "id" => 3,
+    //     "name" => "IOMA - Instituto Obra Médico Asistencial",
+    //     "img" => "assets/images/obras-sociales/IOMA.png",
+    //   ],
+    //   [
+    //     "id" => 4,
+    //     "name" => "OSDE - Organización de Servicios Directos Empresarios",
+    //     "img" => "assets/images/obras-sociales/OSDE.png",
+    //   ],
+    //   [
+    //     "id" => 5,
+    //     "name" => "PAMI - Programa de Atención Médica Integral",
+    //     "img" => "assets/images/obras-sociales/PAMI.svg",
+    //   ],
+    //   [
+    //     "id" => 6,
+    //     "name" => "Medife",
+    //     "img" => "assets/images/obras-sociales/Medife.jpg",
+    //   ]
+    // ];
+    $obrasSociales = $this->queryBuilder->select($this->table);
     $obrasSocialesCollection = [];
     foreach ($obrasSociales as $obraSocial) {
       $obrasSocialesInstance = new ObraSocial;
